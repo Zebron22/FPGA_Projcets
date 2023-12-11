f8a979cafd71a950eb2838344a74a0f779aa1b56
Cebron Williams <cebronwilliams@gmail.com>
2023-12-11 00:52:27 -0500

    Update top.vhd

--- VGA_project/LCD_example/top.vhd
+++ VGA_project/LCD_example/top.vhd
@@ -1,7 +1,6 @@
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
-use IEEE.STD_LOGIC_ARITH.ALL;
-use IEEE.STD_LOGIC_UNSIGNED.ALL;
+
 
 entity top is
 		Port ( DB : out std_logic_vector(7 downto 4); --DB : out std_logic_vector(3 downto 0);
